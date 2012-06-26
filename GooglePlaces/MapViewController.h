67e79b378a5486bc98b4f06858388c2cebb64790
Diarmuid O'Keeffe <iamdermo@gmail.com>
2012-06-26 15:38:05 -0700

    iPhone Mapkit Tutorial

--- GooglePlaces/MapViewController.h
+++ GooglePlaces/MapViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "MapPoint.h"
 
-@interface MapViewController : UIViewController
+#define kGOOGLE_API_KEY @"AIzaSyBGdA4d_LCRUKLAlY_2sKhiEt_KsPpzb0E"
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 
+@interface MapViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
+{
+    CLLocationManager *locationManager;
+    CLLocationCoordinate2D currentCentre;
+    int currenDist;
+    BOOL firstLaunch;
+}
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @end
